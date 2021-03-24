02ad49ac1909da291e2eec24cf00e2290120c3b5
DwarfDev <74852537+DwarflinDeveloping@users.noreply.github.com>
2021-03-24 13:27:41 +0100

    Update guild_family.py

--- guild_family.py
+++ guild_family.py
@@ -1,21 +1,35 @@
-async def set_family(ctx, *args):
-    if not ctx.channel.permissions_for(ctx.author).manage_messages:
-        await ctx.send("You are not allowed to change the default guild url.\n"
-                       "Confused? Try `%troubble changefamily`")
-        return
-    guild_family_file = open(f"data/guild_familys/{str(ctx.guild.id)}.txt", "w")
-    guild_family_file.write(args[1])
-    guild_family_file.close()
+def set_family(guild_id, family_url):
+    from utils import ReturnCodes
 
-    await ctx.send(f"The default URL of the guild has been changed to```{args[1]}```")
+    try:
+        guild_family_file = open(f"data/guild_familys/{str(guild_id)}.txt", "w")
+        guild_family_file.write(family_url)
+        guild_family_file.close()
+
+        return ReturnCodes.SUCCESS
+    except:
+        return ReturnCodes.OTHER_ERROR
 
 
-async def clear_family(guild_id):
+def clear_family(guild_id):
     from utils import ReturnCodes
 
     import os
     if os.path.isfile(f"data/guild_familys/{str(guild_id)}.txt"):
         os.remove(f"data/guild_familys/{str(guild_id)}.txt")
         return ReturnCodes.SUCCESS
+    else:
+        return ReturnCodes.NOT_FOUND
+
+
+def get_family(guild_id):
+    from utils import ReturnCodes
+    import os
+
+    if os.path.isfile(f"data/guild_familys/{str(guild_id)}.txt"):
+        guild_family_file = open(f"data/guild_familys/{str(guild_id)}.txt", "r")
+        guild_family = guild_family_file.read()
+        guild_family_file.close()
+        return guild_family
     else:
         return ReturnCodes.NOT_FOUND
