43d72be276f857582668e86edd72ce5d8a20c28a
DwarfDev <74852537+DwarflinDeveloping@users.noreply.github.com>
2021-03-25 14:51:18 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,6 +3,16 @@ import asyncio
 from enum import Enum
 
 
+class FamilyTemplates:
+    YOUTUBE = "https://www.youtube.com/%ARTICLE%"
+    TWITCH = "https://twitch.tv/%ARTICLE%"
+
+    MINECRAFT = "https://minecraft.fandom.com/%WIKI%/wiki/%ARTICLE%"
+    FANDOM = "https://%WIKI%.fandom.com/wiki/%ARTICLE%"
+    GAMEPEDIA = "https://%WIKI%.gamepedia.com/%ARTICLE%"
+    WIKIPEDIA = "https://%WIKI%.wikipedia.org/wiki/%ARTICLE%"
+
+
 class ReturnCodes(Enum):
     SUCCESS = 0
     CANCELLED = 1
@@ -12,10 +22,36 @@ class ReturnCodes(Enum):
     PERMISSION_ERROR = 5
     NOT_FOUND = 6
     PARAMTER_ERROR = 7
+    VARIABLE_INVAILED = 8
+    NO_CHANGES = 9
 
 
 class Cancelled(Exception):
     pass
+
+
+def get_family_template(search):
+    search = search.lower()
+    if search.startswith("wikipedia-"):
+        return FamilyTemplates.WIKIPEDIA.replace("%WIKI%", search.replace("mcw-", ""))
+
+    elif search.startswith("minecraft-"):
+        return FamilyTemplates.MINECRAFT.replace("%WIKI%", search.replace("minecraft-", ""))
+
+    elif search.startswith("fandom-"):
+        return FamilyTemplates.MINECRAFT.replace("%WIKI%", search.replace("fandom-", ""))
+
+    elif search.startswith("gamepedia-"):
+        return FamilyTemplates.MINECRAFT.replace("%WIKI%", search.replace("gamepedia-", ""))
+
+    elif search == "youtube":
+        return FamilyTemplates.YOUTUBE
+
+    elif search == "twitch":
+        return FamilyTemplates.TWITCH
+
+    else:
+        return ReturnCodes.NOT_FOUND
 
 
 async def get_confirmation(confirmation_message, confirmation_user, client):
