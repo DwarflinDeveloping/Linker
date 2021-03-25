f758a5ef3fee502a6f5a9b692ff6a5a6f549021f
DwarfDev <74852537+DwarflinDeveloping@users.noreply.github.com>
2021-03-25 17:55:14 +0100

    Update custom_family.py

--- custom_family.py
+++ custom_family.py
@@ -1,25 +1,11 @@
 def manage_search(search):
+    from utils import get_family_template, ReturnCodes
     search_family = search.split(":")[0].replace("$", "")
-    if search_family == "mc":
-        search_family = "minecraft-de.gamepedia.com"
-    elif search_family == "mcen":
-        search_family = "minecraft-en.gamepedia.com"
-    elif search_family.startswith("w-"):
-        wikipedia_family = search.split(":")[0].split("w-")[1]
-        if len(wikipedia_family) != 2:
-            return
-        search_family = f"{wikipedia_family}.wikipedia.org/wiki"
-    elif search_family.startswith("gp-"):
-        gamepedia_family = search.split(":")[0].split("gp-")[1]
-        search_family = f"{gamepedia_family}.gamepedia.com"
-    elif search_family.startswith("f-"):
-        gamepedia_family = search.split(":")[0].split("f-")[1]
-        search_family = f"{gamepedia_family}.fandom.com"
-    elif search.split(":")[0] == "mctw":
-        search_family = "technik-de.gamepedia.com"
-    else:
-        search_family = search.split(":")[0]
+    get_progess = get_family_template(search_family)
     search_request = search.split(":")[1]
-    search_url = f"https://{search_family}/{search_request}"
+    if get_progess != ReturnCodes.NOT_FOUND:
+        search_url = get_progess.replace("%ARTICLE%", search_request)
+    else:
+        search_url = f"https://{search_family}/{search_request}"
 
     return search_url
