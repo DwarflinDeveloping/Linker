3620bd7a33bb621ebd55753e5ae0407c24723b2a
DwarfDev <74852537+DwarflinDeveloping@users.noreply.github.com>
2021-03-25 14:47:26 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,16 +2,26 @@ import discord
 
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="%")
+bot = commands.Bot(command_prefix="%", intents=discord.Intents.all())
+bot.help_command = None
 
 
 @bot.listen("on_message")
 async def send_link(message):
-    if "[[" not in message.content or message.author.bot:
+    if "{" not in message.content or message.author.bot:
         return
     from search import manage_search
     link_content = manage_search(message)
+    if len(link_content) > 2000:
+        await message.channel.send("You included too many links in this message!")
+        return
     await message.channel.send(link_content)
+
+
+@bot.command()
+async def help(ctx, *args):
+    from help import manage_request
+    await manage_request(ctx, *args)
 
 
 @bot.command()
@@ -23,23 +33,8 @@ async def admin(ctx, *args):
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching,
-                                                        name="%troubble"))
+                                                        name="%help"))
     print("Logged on as", bot.user)
-
-
-@bot.command()
-async def troubble(ctx, *args):
-    if len(args) == 0:
-        await ctx.send(
-            "**help articles:**\n"
-            "`%troubble changefamily`")
-    elif args[0] == "changefamily":
-        # TODO Link
-        await ctx.send("To change the default url of a guild you need the \"manage_messages\" permission.\n\n"
-                       "Still in trouble? LINK")
-        return
-    else:
-        await ctx.send("I could not find this article.")
 
 
 @bot.command()
