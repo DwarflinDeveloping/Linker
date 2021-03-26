0725948b44c378ef903ea6a73fc4a68bc7d7a101
DwarfDev <74852537+DwarflinDeveloping@users.noreply.github.com>
2021-03-26 18:31:16 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import discord
+from discord.ext import commands as cmds
 
-from discord.ext import commands
+import os
 
-bot = commands.Bot(command_prefix="%", intents=discord.Intents.all())
+bot = cmds.Bot(command_prefix="%", intents=discord.Intents.all())
 bot.help_command = None
 
 
@@ -20,13 +21,19 @@ async def send_link(message):
 
 @bot.command()
 async def help(ctx, *args):
-    from help import manage_request
-    await manage_request(ctx, *args)
+    from commands import help
+    await help.manage_request(ctx, *args)
 
 
 @bot.command()
 async def admin(ctx, *args):
-    from admincmds import manage_request
+    from commands.admincmds import manage_request
+    await manage_request(ctx, *args)
+
+
+@bot.command()
+async def op(ctx, *args):
+    from commands.manage_op import manage_request
     await manage_request(ctx, *args)
 
 
@@ -38,21 +45,27 @@ async def on_ready():
 
 
 @bot.command()
-async def op(ctx, *args):
-    from manage_op import manage_request
+async def userwords(ctx, *args, client=bot):
+    from commands.custom_words_user import manage_request
     await manage_request(ctx, *args)
 
 
 @bot.command()
+async def guildwords(ctx, *args):
+    from commands.custom_words_guild import manage_request
+    await manage_request(ctx, *args, client=bot)
+
+
+@bot.command()
 async def guildfamily(ctx, *args):
-    from guild_family import manage_request
+    from commands.guild_family import manage_request
     await manage_request(ctx, *args, client=bot)
 
 
 @bot.command()
 async def userfamily(ctx, *args):
-    from user_family import manage_request
+    from commands.user_family import manage_request
     await manage_request(ctx, *args, client=bot)
 
 
-bot.run("TOKEN")
+bot.run(os.getenv("TOKEN"))
