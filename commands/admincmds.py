d2597c2e1f459f291d8d4d4a54bdc94355caf7f8
DwarfDev <74852537+DwarflinDeveloping@users.noreply.github.com>
2021-04-04 11:21:07 +0200

    Update admincmds.py

--- commands/admincmds.py
+++ commands/admincmds.py
@@ -1,3 +1,65 @@
+administrators = [784473264755834880]
+operators = [784473264755834880]
+
+
+paused = False
+
+
+async def pause(ctx, client):
+    from embeds import create_custom_embed
+    from discord import Colour
+
+    if ctx.author.id != 784473264755834880:
+        embed = create_custom_embed(
+            embed_message="You are not permitted to close the bot!",
+            user=ctx.author,
+            colour=Colour.dark_red()
+        )
+        await ctx.send(embed=embed)
+        return
+
+    global paused
+    if paused:
+        await ctx.send(
+            embed=create_custom_embed(
+                embed_message="I stopped my pause.",
+                user=ctx.author,
+                colour=Colour.dark_green()
+            )
+        )
+        paused = False
+        return
+
+    confirmation_message = await ctx.send(
+        embed=create_custom_embed(
+            embed_title="Confirmation",
+            embed_message="You're about to pause me. Do you really want to continue this process?",
+            user=ctx.author,
+            colour=Colour.blue()
+        )
+    )
+    from utils import ReturnCodes, get_confirmation
+    confirmation = await get_confirmation(confirmation_message, ctx.author, client)
+    if confirmation == ReturnCodes.SUCCESS:
+        await ctx.send(
+            embed=create_custom_embed(
+                embed_message="I paused. Until you repeat this command, I will not answer any requests.",
+                user=ctx.author,
+                colour=Colour.dark_green()
+            )
+        )
+        paused = True
+    elif confirmation == ReturnCodes.CANCELLED:
+        await confirmation_message.delete()
+
+    elif confirmation == ReturnCodes.TIMEOUT_ERROR:
+        await confirmation_message.delete()
+
+    elif confirmation == ReturnCodes.OTHER_ERROR:
+        from embeds import handle_error
+        await ctx.send(handle_error(confirmation, ctx.author))
+
+
 async def shutdown(ctx, client):
     from embeds import create_custom_embed
     from discord import Colour
@@ -37,21 +99,3 @@ async def shutdown(ctx, client):
     elif confirmation == ReturnCodes.OTHER_ERROR:
         from embeds import handle_error
         await ctx.send(handle_error(confirmation, ctx.author))
-
-
-async def manage_request(ctx, *args):
-    args = [arg.lower() for arg in args]
-    if len(args) != 1:
-        # TODO Syntax Error
-        await ctx.send("SYNTAX ERROR!")
-    if args[0] == "guildfam":
-        from guild_family import get_family
-        from utils import ReturnCodes
-
-        get_family_process = get_family(ctx.guild.id)
-        if get_family_process == ReturnCodes.NOT_FOUND:
-            await ctx.send("This guild has no default url.")
-        else:
-            await ctx.send(f"The default url of this guild is:```{get_family_process}```")
-    if args[0] == "userfam":
-        pass
