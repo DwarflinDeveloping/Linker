ce0ccab6706a065aea0b67898d6ce59dcc15e71a
DwarfDev <74852537+DwarflinDeveloping@users.noreply.github.com>
2021-05-19 13:27:20 +0200

    added a token check

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# enter the bot token below!
 export TOKEN=bottoken
+
+if [ $TOKEN == "bottoken" ];
+then
+  echo You have not set the token. Set it in line 4 and try again.
+  exit 1
+fi
+
 ./bot.py
